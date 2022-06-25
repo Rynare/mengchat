53a9313877ad7eb833c3f6ca25433a4aedcd8f79
Rynare <106422092+Rynare@users.noreply.github.com>
2022-06-25 11:17:23 +0700

    Add files via upload

---
+++ script/setup/firebase_config.js
@@ -0,0 +1,41 @@
+const _0x167732 = _0x25a6;
+
+function _0x25a6(_0x1fd3f0, _0x222146) {
+    const _0x107682 = _0x1076();
+    return _0x25a6 = function (_0x25a645, _0x120f6d) {
+        _0x25a645 = _0x25a645 - 0xf6;
+        let _0x559d29 = _0x107682[_0x25a645];
+        return _0x559d29;
+    }, _0x25a6(_0x1fd3f0, _0x222146);
+}(function (_0x5e9434, _0x4c471a) {
+    const _0x275b9f = _0x25a6,
+        _0x1fc9fd = _0x5e9434();
+    while (!![]) {
+        try {
+            const _0x609c05 = parseInt(_0x275b9f(0xf7)) / 0x1 + parseInt(_0x275b9f(0xf6)) / 0x2 * (-parseInt(_0x275b9f(0x107)) / 0x3) + -parseInt(_0x275b9f(0xfc)) / 0x4 + parseInt(_0x275b9f(0xfe)) / 0x5 + parseInt(_0x275b9f(0xf8)) / 0x6 * (parseInt(_0x275b9f(0xff)) / 0x7) + parseInt(_0x275b9f(0x106)) / 0x8 * (parseInt(_0x275b9f(0x102)) / 0x9) + parseInt(_0x275b9f(0xf9)) / 0xa;
+            if (_0x609c05 === _0x4c471a) break;
+            else _0x1fc9fd['push'](_0x1fc9fd['shift']());
+        } catch (_0x363989) {
+            _0x1fc9fd['push'](_0x1fc9fd['shift']());
+        }
+    }
+}(_0x1076, 0x79c41));
+const firebaseConfig = {
+        'apiKey': 'AIzaSyDYDeSdcqzyT6DwIRskEBClU4IZJwQmocs',
+        'authDomain': _0x167732(0xfb),
+        'databaseURL': _0x167732(0x100),
+        'projectId': _0x167732(0xfd),
+        'storageBucket': _0x167732(0xfa),
+        'messagingSenderId': _0x167732(0x104),
+        'appId': _0x167732(0x105)
+    },
+    app = firebase[_0x167732(0x101)](firebaseConfig),
+    database = firebase[_0x167732(0x103)]();
+
+function _0x1076() {
+    const _0x3b5f31 = ['6698547PBayvz', 'database', '1029338288410', '1:1029338288410:web:b857f74906e94cd4a65ca3', '8fBAula', '1893876XMNunX', '2ZnJVxD', '116875UtsPBj', '106668cDxWGi', '1502340VJeCEe', 'mengchat-2a764.appspot.com', 'mengchat-2a764.firebaseapp.com', '1894652gMKETE', 'mengchat-2a764', '2517130iEHucF', '35aQsUwJ', 'https://mengchat-2a764-default-rtdb.firebaseio.com', 'initializeApp'];
+    _0x1076 = function () {
+        return _0x3b5f31;
+    };
+    return _0x1076();
+}
